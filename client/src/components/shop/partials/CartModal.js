170770babf248fda70e4259910060cfa48eedf7f
dankk <dankkmemes@gmail.com>
2021-09-22 19:47:22 +0300

    width cart change

--- client/src/components/shop/partials/CartModal.js
+++ client/src/components/shop/partials/CartModal.js
@@ -129,7 +129,7 @@ const CartModal = (props) => {
                     //https://codesandbox.io/s/oz8wb?file=/src/index.js:356-489
                     e.stopPropagation();
                 }}
-                    className={`${isCartModalOpen() ? 'translate-x-0 ease-out' : 'translate-x-full ease-in'} fixed right-0 top-0 max-w-xs w-full h-full px-6 py-4 transition duration-300 transform overflow-y-auto bg-white border-l-2 border-gray-300`}>
+                    className={`${isCartModalOpen() ? 'translate-x-0 ease-out' : 'translate-x-full ease-in'} fixed right-0 top-0 max-w-xs w-80 sm:w-full h-full px-6 py-4 transition duration-300 transform overflow-y-auto bg-white border-l-2 border-gray-300`}>
                     <div className="flex items-center justify-between">
                         <h3 className="text-2xl font-medium text-gray-700">Your cart</h3>
                         <button onClick={cartModalOpen} className="text-gray-600 focus:outline-none">
