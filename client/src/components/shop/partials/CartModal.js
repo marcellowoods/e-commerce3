6cebc72709b9a06c7967fcdbd5395ed7e4baedaa
dankk <dankkmemes@gmail.com>
2021-09-22 11:59:48 +0300

    added svg

--- client/src/components/shop/partials/CartModal.js
+++ client/src/components/shop/partials/CartModal.js
@@ -24,6 +24,7 @@ const items = [
         pImages: ["https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"]
     }
 ];
+const products = items;
 const quantity = (item) => item.quantity;
 const subTotal = (id, price) => price;
 const removeCartProduct = (id) => { };
@@ -58,12 +59,14 @@ const CartModal = (props) => {
     const history = useHistory();
 
     const { data, dispatch } = useContext(LayoutContext);
-    const products = data.cartProduct;
+    // const products = data.cartProduct;
 
     const cartModalOpen = () =>
         dispatch({ type: "cartModalToggle", payload: !data.cartModal });
 
     const isCartModalOpen = () => data.cartModal == true;
+
+    const hasProducts = () => products && products.length !== 0;
 
     // useEffect(() => {
     //     fetchData();
@@ -111,18 +114,28 @@ const CartModal = (props) => {
                     </button>
                 </div>
                 <hr className="my-3" />
-                {/* price, name, quantity, imageLink, removeCartProduct */}
-                {items.map((item, index) => (
-                    <CartModalItem
-                        key={index}
-                        price={item.pPrice}
-                        name={item.pName}
-                        quantity={item.quantity}
-                        imageLink={item.pImages[0]}
-                    />
-                ))}
 
-                <div className="mt-8">
+                {hasProducts() &&
+                    products.map((item, index) => (
+                        <CartModalItem
+                            key={index}
+                            price={item.pPrice}
+                            name={item.pName}
+                            quantity={item.quantity}
+                            imageLink={item.pImages[0]}
+                        />
+                    )
+                    )}
+
+                {hasProducts() &&
+                    <a className="flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                        <svg className="transform rotate-180 h-5 w-5 mx-2" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
+                        <span>Continue shopping</span>
+                        {/* <svg className="h-5 w-5 mx-2" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg> */}
+                    </a>}
+
+                {/* promo code */}
+                {/* <div className="mt-8">
                     <form className="flex items-center justify-center">
                         <input className="form-input w-48" type="text" placeholder="Add promocode">
                         </input>
@@ -130,7 +143,7 @@ const CartModal = (props) => {
                             <span>Apply</span>
                         </button>
                     </form>
-                </div>
+                </div> */}
                 <a className="flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
                     <span>Chechout</span>
                     <svg className="h-5 w-5 mx-2" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
@@ -142,65 +155,156 @@ const CartModal = (props) => {
 
 export default CartModal;
 
-
-
-// {
-//     products &&
-//         products.length !== 0 &&
-//         products.map((item, index) => {
-//             return (
-//                 <Fragment key={index}>
-//                     {/* Cart Product Start */}
-//                     <div className="text-white flex space-x-2 my-4 items-center">
-//                         <img
-//                             className="w-16 h-16 object-cover object-center"
-//                             src={`${apiURL}/uploads/products/${item.pImages[0]}`}
-//                             alt="cartProduct"
-//                         />
-//                         <div className="relative w-full flex flex-col">
-//                             <div className="my-2">{item.pName}</div>
-//                             <div className="flex items-center justify-between">
-//                                 <div className="flex items-center justify-between space-x-2">
-//                                     <div className="text-sm text-gray-400">
-//                                         Quantity :
-//                                     </div>
-//                                     <div className="flex items-end">
-//                                         <span className="text-sm text-gray-200">
-//                                             {quantity(item._id)}
-//                                         </span>
-//                                     </div>
-//                                 </div>
-//                                 <div>
-//                                     {" "}
-//                                     <span className="text-sm text-gray-400">
-//                                         Subtotoal :
-//                                     </span>{" "}
-//                                     ${subTotal(item._id, item.pPrice)}.00
-//                                 </div>{" "}
-//                                 {/* SUbtotal Count */}
-//                             </div>
-//                             {/* Cart Product Remove Button */}
-//                             <div
-//                                 onClick={(e) => removeCartProduct(item._id)}
-//                                 className="absolute top-0 right-0 text-white"
+// let f = () => (
+//     <Fragment>
+//         {/* Black Overlay */}
+//         <div
+//             className={`${!data.cartModal ? "hidden" : ""
+//                 } fixed top-0 z-30 w-full h-full bg-black opacity-50`}
+//         />
+//         {/* Cart Modal Start */}
+//         <section
+//             className={`${!data.cartModal ? "hidden" : ""
+//                 } fixed z-40 inset-0 flex items-start justify-end`}
+//         >
+//             <div
+//                 style={{ background: "#303031" }}
+//                 className="w-full md:w-5/12 lg:w-4/12 h-full flex flex-col justify-between"
+//             >
+//                 <div className="overflow-y-auto">
+//                     <div className="border-b border-gray-700 flex justify-between">
+//                         <div className="p-4 text-white text-lg font-semibold">Cart</div>
+//                         {/* Cart Modal Close Button */}
+//                         <div className="p-4 text-white">
+//                             <svg
+//                                 onClick={(e) => cartModalOpen()}
+//                                 className="w-6 h-6 cursor-pointer"
+//                                 fill="currentColor"
+//                                 viewBox="0 0 20 20"
+//                                 xmlns="http://www.w3.org/2000/svg"
 //                             >
-//                                 <svg
-//                                     className="w-5 h-5 cursor-pointer"
-//                                     fill="currentColor"
-//                                     viewBox="0 0 20 20"
-//                                     xmlns="http://www.w3.org/2000/svg"
-//                                 >
-//                                     <path
-//                                         fillRule="evenodd"
-//                                         d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-//                                         clipRule="evenodd"
-//                                     />
-//                                 </svg>
-//                             </div>
+//                                 <path
+//                                     fillRule="evenodd"
+//                                     d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+//                                     clipRule="evenodd"
+//                                 />
+//                             </svg>
 //                         </div>
 //                     </div>
-//                     {/* Cart Product Start */}
-//                 </Fragment>
-//             );
-//         })
-// }
+//                     <div className="m-4 flex-col">
+//                         {products &&
+//                             products.length !== 0 &&
+//                             products.map((item, index) => {
+//                                 return (
+//                                     <Fragment key={index}>
+//                                         {/* Cart Product Start */}
+//                                         <div className="text-white flex space-x-2 my-4 items-center">
+//                                             <img
+//                                                 className="w-16 h-16 object-cover object-center"
+//                                                 src={`${apiURL}/uploads/products/${item.pImages[0]}`}
+//                                                 alt="cartProduct"
+//                                             />
+//                                             <div className="relative w-full flex flex-col">
+//                                                 <div className="my-2">{item.pName}</div>
+//                                                 <div className="flex items-center justify-between">
+//                                                     <div className="flex items-center justify-between space-x-2">
+//                                                         <div className="text-sm text-gray-400">
+//                                                             Quantity :
+//                                                         </div>
+//                                                         <div className="flex items-end">
+//                                                             <span className="text-sm text-gray-200">
+//                                                                 {quantity(item._id)}
+//                                                             </span>
+//                                                         </div>
+//                                                     </div>
+//                                                     <div>
+//                                                         {" "}
+//                                                         <span className="text-sm text-gray-400">
+//                                                             Subtotoal :
+//                                                         </span>{" "}
+//                                                         ${subTotal(item._id, item.pPrice)}.00
+//                                                     </div>{" "}
+//                                                     {/* SUbtotal Count */}
+//                                                 </div>
+//                                                 {/* Cart Product Remove Button */}
+//                                                 <div
+//                                                     onClick={(e) => removeCartProduct(item._id)}
+//                                                     className="absolute top-0 right-0 text-white"
+//                                                 >
+//                                                     <svg
+//                                                         className="w-5 h-5 cursor-pointer"
+//                                                         fill="currentColor"
+//                                                         viewBox="0 0 20 20"
+//                                                         xmlns="http://www.w3.org/2000/svg"
+//                                                     >
+//                                                         <path
+//                                                             fillRule="evenodd"
+//                                                             d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+//                                                             clipRule="evenodd"
+//                                                         />
+//                                                     </svg>
+//                                                 </div>
+//                                             </div>
+//                                         </div>
+//                                         {/* Cart Product Start */}
+//                                     </Fragment>
+//                                 );
+//                             })}
+
+//                         {products === null && (
+//                             <div className="m-4 flex-col text-white text-xl text-center">
+//                                 No product in cart
+//                             </div>
+//                         )}
+//                     </div>
+//                 </div>
+//                 <div className="m-4 space-y-4">
+//                     <div
+//                         onClick={(e) => cartModalOpen()}
+//                         className="cursor-pointer px-4 py-2 border border-gray-400 text-white text-center cursor-pointer"
+//                     >
+//                         Continue shopping
+//                     </div>
+//                     {data.cartTotalCost ? (
+//                         <Fragment>
+//                             {isAuthenticate() ? (
+//                                 <div
+//                                     className="px-4 py-2 bg-black text-white text-center cursor-pointer"
+//                                     onClick={(e) => {
+//                                         history.push("/checkout");
+//                                         cartModalOpen();
+//                                     }}
+//                                 >
+//                                     Checkout ${data.cartTotalCost}.00
+//                                 </div>
+//                             ) : (
+//                                 <div
+//                                     className="px-4 py-2 bg-black text-white text-center cursor-pointer"
+//                                     onClick={(e) => {
+//                                         history.push("/");
+//                                         cartModalOpen();
+//                                         dispatch({
+//                                             type: "loginSignupError",
+//                                             payload: !data.loginSignupError,
+//                                         });
+//                                         dispatch({
+//                                             type: "loginSignupModalToggle",
+//                                             payload: !data.loginSignupModal,
+//                                         });
+//                                     }}
+//                                 >
+//                                     Checkout ${data.cartTotalCost}.00
+//                                 </div>
+//                             )}
+//                         </Fragment>
+//                     ) : (
+//                         <div className="px-4 py-2 bg-black text-white text-center cursor-not-allowed">
+//                             Checkout
+//                         </div>
+//                     )}
+//                 </div>
+//             </div>
+//         </section>
+//         {/* Cart Modal End */}
+//     </Fragment>
+// )
