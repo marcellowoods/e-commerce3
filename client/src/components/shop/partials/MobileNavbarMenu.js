111685db0754bd19ff0ebb05e22927d6d9623997
dankk <dankkmemes@gmail.com>
2021-09-24 12:11:13 +0300

    asdf

--- client/src/components/shop/partials/MobileNavbarMenu.js
+++ client/src/components/shop/partials/MobileNavbarMenu.js
@@ -22,23 +22,7 @@ const MobileNavbarMenu = (props) => {
             <div>
                 <div
                     className={`${isMobileNavOpen() ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'} z-40 fixed left-0 top-0 max-w-xs w-80 sm:w-full h-full px-6 py-4 transition duration-300 transform overflow-y-auto bg-white  `}>
-                    {/* <div className="flex items-center justify-between">
-                        <h3 className="text-2xl font-medium text-gray-700">Your cart</h3>
-                        <button onClick={closeMobileNav} className="text-gray-600 focus:outline-none">
-                            <svg className="h-5 w-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M6 18L18 6M6 6l12 12"></path></svg>
-                        </button>
-                    </div> */}
-
-
-                    {/* <a  className={`cursor-pointer flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500`}>
-                        <span>
-                            Checkout
-                        </span>
-                        <svg className="h-5 w-5 mx-2" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-                    </a> */}
-
                     <nav className={`p-5`}>
-
                         <div className="flex flex-col">
                             <a className="mt-3 color-main-light hover:underline sm:mx-3 sm:mt-0" href="#">Home</a>
                             <a className="mt-3 color-main-light hover:underline sm:mx-3 sm:mt-0" href="#">Shop</a>
@@ -48,7 +32,6 @@ const MobileNavbarMenu = (props) => {
                         </div>
                     </nav>
                 </div>
-
                 {isMobileNavOpen() &&
                     <div
                         className={"modal-backdrop"}
@@ -58,9 +41,6 @@ const MobileNavbarMenu = (props) => {
                         }}
                     ></div>}
             </div>
-
-
-
         </Fragment >
     );
 };
