7ef3e1dc5b50b42c96f4eae99ab9d0d1b10dff34
dankk <dankkmemes@gmail.com>
2021-09-25 12:05:07 +0300

    ProductByCategory

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React, { Fragment, useReducer } from "react";
-import Navbar from './components/shop/partials/Navbar'
-import MobileNavbarMenu from './components/shop/partials/MobileNavbarMenu'
-import CartModal from './components/shop/partials/CartModal'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { LayoutContext, layoutState, layoutReducer } from "./components/shop";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductByCategory from "./components/shop/home/ProductByCategory";
 
 function App() {
     const [data, dispatch] = useReducer(layoutReducer, layoutState);
@@ -11,89 +9,7 @@ function App() {
         <Fragment>
             <LayoutContext.Provider value={{ data, dispatch }}>
                 <Router>
-                    <Navbar />
-                    <MobileNavbarMenu />
-                    <CartModal />
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <div className=" mx-auto">
-                        <img className=" object-cover rounded" src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80" alt="" />
-                        <img className=" object-cover rounded" src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80" alt="" />
-                    </div>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-                    <h1 className="relative py-3 max-w-xs md:max-w-lg mx-auto">
-                        product
-                    </h1>
-
+                <ProductByCategory/>
 
                 </Router>
             </LayoutContext.Provider>
@@ -102,3 +18,29 @@ function App() {
 }
 
 export default App;
+
+// import React, { Fragment, useReducer } from "react";
+// import ProductByCategory from "./components/shop/home/ProductByCategory";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { LayoutContext, layoutState, layoutReducer } from "./components/shop";
+
+// function App() {
+//     const [data, dispatch] = useReducer(layoutReducer, layoutState);
+//     return (
+//         <Fragment>
+//             <LayoutContext.Provider value={{ data, dispatch }}>
+//                 <Router>
+                    
+//                    <p>ef</p>
+
+//                 </Router>
+//             </LayoutContext.Provider>
+//         </Fragment>
+//     );
+// }
+
+// export default App;
+
+
+
+
--- client/src/components/shop/layout/index.js
+++ client/src/components/shop/layout/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment, createContext } from "react";
 // import { Navber, Footer, CartModal } from "../partials";
 // import LoginSignup from "../auth/LoginSignup";
+import { Navbar, CartModal, MobileNavbarMenu } from "../partials";
 
 export const LayoutContext = createContext();
 
@@ -8,9 +9,10 @@ const Layout = ({ children }) => {
   return (
     <Fragment>
       <div className="flex-grow">
-        {/* <Navber /> */}
+        <Navbar />
         {/* <LoginSignup /> */}
-        {/* <CartModal /> */}
+        <CartModal />
+        <MobileNavbarMenu />
         {/* All Children pass from here */}
         {children}
       </div>
