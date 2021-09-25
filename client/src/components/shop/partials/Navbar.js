f79cb933b18cd4379d1a29f70f4585190e0efab1
dankk <dankkmemes@gmail.com>
2021-09-25 10:56:33 +0300

    added SearchBar

--- client/src/components/shop/partials/Navbar.js
+++ client/src/components/shop/partials/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useContext } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import NavigationItems from './NavigationItems';
+import SearchBar from './SearchBar.js'
 import "./style.css";
 
 // import { logout } from "./Action";
@@ -63,16 +64,7 @@ const NavbarRender = ({ mobileNavbarToggle, loginModalToggle, cartModalToggle })
                 <nav className={`hidden sm:flex sm:justify-center sm:items-center `}>
                     <NavigationItems />
                 </nav>
-                <div className="hidden sm:flex relative py-5  max-w-lg mx-auto">
-                    <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
-                        <svg className="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
-                            <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                        </svg>
-                    </span>
-
-                    <input className="w-full color-main-bold border color-main-bg rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline" type="text" placeholder="Search">
-                    </input>
-                </div>
+                <SearchBar cname={"hidden sm:flex relative py-5  max-w-lg mx-auto"} />
             </div>
             {/* End Navber Section */}
         </Fragment >
