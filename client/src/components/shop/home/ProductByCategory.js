7f902a1e9bcc2f28613e67e347a7cd5161aeef9e
dankk <dankkmemes@gmail.com>
2021-09-26 16:50:29 +0300

    productcard styling bg

--- client/src/components/shop/home/ProductByCategory.js
+++ client/src/components/shop/home/ProductByCategory.js
@@ -127,13 +127,14 @@ const ProductCard = ({ id, name, price, imageUrl, onAddClick }) => {
 
     return (
         <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
-            <div className="flex items-end justify-end h-56 w-full bg-cover" style={{ backgroundImage: `url(${imageUrl})` }}>
+            {/* bg-cover */}
+            <div className="flex items-end justify-end h-56 w-full bg-cover bg-center" style={{ backgroundImage: `url(${imageUrl})` }}>
                 <button onClick={(id => onAddClick(id))} className="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
                     <svg className="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
                 </button>
             </div>
             <div className="px-5 py-3">
-                <h3 className="text-gray-700 uppercase">{name}</h3>
+                <h3 className="color-main-light uppercase">{name}</h3>
                 <span className="text-gray-500 mt-2">${price}</span>
             </div>
         </div>
@@ -150,6 +151,15 @@ const PageComponent = () => {
 
     const fetchData = () => {
         let items = [];
+
+        items.push({
+            id: Math.floor(Math.random() * 1000),
+            quantity: 4,
+            pName: "watch",
+            pPrice: 100,
+            pImages: ["https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"]
+        })
+
         let item = {
             id: Math.floor(Math.random() * 1000),
             quantity: 2,
@@ -194,7 +204,7 @@ const PageComponent = () => {
                                 </button>
                             </div>
                             <div className="px-5 py-3">
-                                <h3 className="text-gray-700 uppercase">classNameic watch</h3>
+                                <h3 className="text-gray-700 uppercase">basic watch</h3>
                                 <span className="text-gray-500 mt-2">$123</span>
                             </div>
                         </div>
@@ -205,6 +215,7 @@ const PageComponent = () => {
                                 quantity={3}
                                 imageUrl={p.pImages[0]}
                                 price={p.pPrice}
+                                name={p.pName}
                                 onAddClick={(id) => {}}
                             />
                         ))
