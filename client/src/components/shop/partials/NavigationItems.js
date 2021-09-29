3a3a7eaa0e4fc447779367e5d9736daa4f7e145d
dankk <dankkmemes@gmail.com>
2021-09-29 09:15:04 +0300

    hover:text-indigo

--- client/src/components/shop/partials/NavigationItems.js
+++ client/src/components/shop/partials/NavigationItems.js
@@ -8,11 +8,11 @@ const NavigationItems = () => {
 
     return (
         <div className="flex flex-col sm:flex-row">
-            <a className="mt-3 color-main-light hover:underline sm:mx-3 sm:mt-0" href="#">Home</a>
-            <a className="mt-3 color-main-light hover:underline sm:mx-3 sm:mt-0" href="#">Shop</a>
-            <a className="mt-3 color-main-light hover:underline sm:mx-3 sm:mt-0" href="#">Categories</a>
-            <a className="mt-3 color-main-light hover:underline sm:mx-3 sm:mt-0" href="#">Contact</a>
-            <a className="mt-3 color-main-light hover:underline sm:mx-3 sm:mt-0" href="#">About</a>
+            <a className="mt-3 color-main-light hover:text-indigo-500 sm:mx-3 sm:mt-0" href="#">Home</a>
+            <a className="mt-3 color-main-light hover:text-indigo-500 sm:mx-3 sm:mt-0" href="#">Shop</a>
+            <a className="mt-3 color-main-light hover:text-indigo-500 sm:mx-3 sm:mt-0" href="#">Categories</a>
+            <a className="mt-3 color-main-light hover:text-indigo-500 sm:mx-3 sm:mt-0" href="#">Contact</a>
+            <a className="mt-3 color-main-light hover:text-indigo-500 sm:mx-3 sm:mt-0" href="#">About</a>
         </div>
     );
 };
