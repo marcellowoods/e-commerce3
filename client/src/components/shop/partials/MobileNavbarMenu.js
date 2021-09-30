8938045dd57d64d2b54f33924d0f52eb39e98192
dankk <dankkmemes@gmail.com>
2021-09-30 05:53:16 +0300

    no

--- client/src/components/shop/partials/MobileNavbarMenu.js
+++ client/src/components/shop/partials/MobileNavbarMenu.js
@@ -1,80 +1,8 @@
-import React, { Fragment, useContext, useEffect, useState } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import NavigationItems from './NavigationItems';
 import { LayoutContext } from "../index";
 import SearchBar from './SearchBar.js'
-import { Dialog, Transition } from '@headlessui/react'
-import { XIcon } from '@heroicons/react/outline'
-
-const Example = () => {
-    const [open, setOpen] = useState(true)
-
-    return (
-        <Transition.Root  show={open} as={Fragment}>
-            <Dialog as="div" className="z-40 fixed inset-0 overflow-hidden" onClose={setOpen}>
-                <div className="absolute inset-0 overflow-hidden">
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-in-out duration-500"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in-out duration-500"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <Dialog.Overlay style={{backgroundColor: 'rgba(0, 0, 0, 0.4)'}} className="absolute inset-0 bg-gray-500  transition-opacity" />
-                    </Transition.Child>
-                    <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex">
-                        <Transition.Child
-                            as={Fragment}
-                            enter="transform transition ease-in-out duration-500 sm:duration-700"
-                            enterFrom="translate-x-full"
-                            enterTo="translate-x-0"
-                            leave="transform transition ease-in-out duration-500 sm:duration-700"
-                            leaveFrom="translate-x-0"
-                            leaveTo="translate-x-full"
-                        >
-                            <div className="relative w-screen max-w-md">
-                                <Transition.Child
-                                    as={Fragment}
-                                    enter="ease-in-out duration-500"
-                                    enterFrom="opacity-0"
-                                    enterTo="opacity-100"
-                                    leave="ease-in-out duration-500"
-                                    leaveFrom="opacity-100"
-                                    leaveTo="opacity-0"
-                                >
-                                    <div className="absolute top-0 left-0 -ml-8 pt-4 pr-2 flex sm:-ml-10 sm:pr-4">
-                                        <button
-                                            type="button"
-                                            className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-                                            onClick={() => setOpen(false)}
-                                        >
-                                            <span className="sr-only">Close panel</span>
-                                            <XIcon className="h-6 w-6" aria-hidden="true" />
-                                        </button>
-                                    </div>
-                                </Transition.Child>
-                                <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
-                                    <div className="px-4 sm:px-6">
-                                        <Dialog.Title className="text-lg font-medium text-gray-900">Panel title</Dialog.Title>
-                                    </div>
-                                    <div className="mt-6 relative flex-1 px-4 sm:px-6">
-                                        {/* Replace with your content */}
-                                        <div className="absolute inset-0 px-4 sm:px-6">
-                                            <div className="h-full border-2 border-dashed border-gray-200" aria-hidden="true" />
-                                        </div>
-                                        {/* /End replace */}
-                                    </div>
-                                </div>
-                            </div>
-                        </Transition.Child>
-                    </div>
-                </div>
-            </Dialog>
-        </Transition.Root>
-    )
-}
 
 const MobileNavbarMenu = (props) => {
 
@@ -93,12 +21,11 @@ const MobileNavbarMenu = (props) => {
 
     return (
         <Fragment>
-            <Example />
             <div className="visible sm:invisible">
                 <div
                     className={`${isMobileNavOpen() ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'} z-40 fixed left-0 top-0 max-w-xs w-80 sm:w-full h-full px-6 py-4 transition duration-300 transform overflow-y-auto bg-white  `}>
                     <div className={`p-3`}>
-                        <SearchBar cname={"mobile-searchbar-w relative py-3  mx-auto"} />
+                        <SearchBar cname={"mobile-searchbar-w relative py-3  mx-auto"}/>
                         <NavigationItems />
                     </div>
                 </div>
