d0853b95edee4ca1abcd4e27bab045f98051103d
dankk <dankkmemes@gmail.com>
2021-10-03 18:27:45 +0300

    loginRender

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,11 +1,36 @@
 import React, { Fragment, useEffect, useState, useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-const Login = () => {
+import { createOrUpdateUser } from "../../functions/auth";
+import { roleBasedRedirect } from "./auxiliary/redirect.js"
+
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup
+} from "firebase/auth";
+
+const auth = getAuth();
+const googleAuthProvider = new GoogleAuthProvider();
+
+const LoginRender = ({
+    googleLogin,
+    email,
+    password,
+    setEmail,
+    setPassword,
+    emailLoginFormSubmit,
+    isEmailLoginDisabled
+}) => {
     return (
         <div className="sm:pt-4">
             <div className="flex bg-white rounded-lg shadow-lg overflow mx-auto sm:max-w-xl">
                 <div className="w-full px-8 sm:py-8 mx-auto">
-                    <a href="#" className="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100">
+                    <button onClick={googleLogin}
+                        href="#" className="w-full flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100">
                         <div className="">
                             <svg className="h-6 w-6" viewBox="0 0 40 40">
                                 <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
@@ -15,37 +40,134 @@ const Login = () => {
                             </svg>
                         </div>
                         <h1 className="px-4 py-3 text-center text-gray-600 font-bold">Sign in with Google</h1>
-                    </a>
-                    <div className="mt-4 flex items-center justify-between">
-                        <span className="border-b w-1/5 lg:w-1/4"></span>
-                        <a href="#" className="text-xs text-center text-gray-500 uppercase">or login with email</a>
-                        <span className="border-b w-1/5 lg:w-1/4"></span>
-                    </div>
-                    <div className="mt-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
-                        <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="email"/>
-                    </div>
-                    <div className="mt-4">
-                        <div className="flex justify-between">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                            <a href="#" className="text-xs text-gray-500">Forget Password?</a>
+                    </button>
+                    <form onSubmit={emailLoginFormSubmit}>
+                        <div className="mt-4 flex items-center justify-between">
+                            <span className="border-b w-1/5 lg:w-1/4"></span>
+                            <a href="#" className="text-xs text-center text-gray-500 uppercase">or login with email</a>
+                            <span className="border-b w-1/5 lg:w-1/4"></span>
                         </div>
-                        <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="password"/>
-                    </div>
-                    <div className="mt-8">
-                        <button className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Login</button>
-                    </div>
-                    <div className="mt-4 flex items-center justify-between">
-                        <span className="border-b w-1/5 md:w-1/4"></span>
-                        <a href="#" className="text-xs text-gray-500 uppercase">or sign up</a>
-                        <span className="border-b w-1/5 md:w-1/4"></span>
-                    </div>
+                        <div className="mt-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
+                            <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="email" />
+                        </div>
+                        <div className="mt-4">
+                            <div className="flex justify-between">
+                                <label className="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                                <a href="#" className="text-xs text-gray-500">Forget Password?</a>
+                            </div>
+                            <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="password" />
+                        </div>
+                        <div className="mt-8">
+                            <button className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600">Login</button>
+                        </div>
+                        <div className="mt-4 flex items-center justify-between">
+                            <span className="border-b w-1/5 md:w-1/4"></span>
+                            <a href="#" className="text-xs text-gray-500 uppercase">or sign up</a>
+                            <span className="border-b w-1/5 md:w-1/4"></span>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     )
 }
 
-export default Login
+const Login = ({ history }) => {
+    const [email, setEmail] = useState("gqlreactnode@gmail.com");
+    const [password, setPassword] = useState("gggggg");
+    const [loading, setLoading] = useState(false);
+    let location = useLocation();
+
+    const { user } = useSelector((state) => ({ ...state }));
+
+    let dispatch = useDispatch();
+
+    useEffect(() => {
+        {
+            if (user && user.token) {
+                let isAdmin = user.role === "admin";
+                roleBasedRedirect(location, history, isAdmin);
+            }
+        }
+
+    }, [user, history]);
+
+    const emailLoginFormSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        // console.table(email, password);
+        try {
+            const result = await signInWithEmailAndPassword(auth, email, password);
+            // console.log(result);
+            const { user } = result;
+            const idTokenResult = await user.getIdTokenResult();
+
+            createOrUpdateUser(idTokenResult.token)
+                .then((res) => {
+                    dispatch({
+                        type: "LOGGED_IN_USER",
+                        payload: {
+                            name: res.data.name,
+                            email: res.data.email,
+                            token: idTokenResult.token,
+                            role: res.data.role,
+                            _id: res.data._id,
+                        },
+                    });
+                    //   roleBasedRedirect(location, history, res);
+                })
+                .catch((err) => console.log(err));
+
+        } catch (error) {
+            console.log(error);
+            // toast.error(error.message);
+            setLoading(false);
+        }
+    };
+
+    const googleLogin = async () => {
+        signInWithPopup(auth, googleAuthProvider)
+            .then(async (result) => {
+                const { user } = result;
+                const idTokenResult = await user.getIdTokenResult();
+                createOrUpdateUser(idTokenResult.token)
+                    .then((res) => {
+                        dispatch({
+                            type: "LOGGED_IN_USER",
+                            payload: {
+                                name: res.data.name,
+                                email: res.data.email,
+                                token: idTokenResult.token,
+                                role: res.data.role,
+                                _id: res.data._id,
+                            },
+                        });
+                        // roleBasedRedirect(location, history, res);
+                    })
+                    .catch((err) => console.log(err));
+            })
+            .catch((err) => {
+                console.log(err);
+                // toast.error(err.message);
+            });
+    };
+
+    return (
+        <LoginRender 
+            googleLogin={googleLogin}
+            email={email}
+            password={password}
+            setEmail={setEmail}
+            setPassword={setPassword}
+            emailLoginFormSubmit={emailLoginFormSubmit}
+            // isEmailLoginDisabled={isEmailLoginDisabled}
+            />
+    );
+};
+
+
+
+export default Login;
 
 
---
+++ client/src/pages/auth/auxiliary/redirect.js
@@ -0,0 +1,19 @@
+//https://reactrouter.com/web/example/auth-workflow
+
+const roleBasedRedirect = (location, history, isAdmin) => {
+
+    if(location && location.state && location.state.from){
+        history.replace(location.state.from);
+
+    } else if (isAdmin) {
+        console.log("push admn")
+        // history.replace("/admin/dashboard");
+    } else {
+        console.log("push history")
+        // history.replace("/user/history");
+    }
+
+}
+
+export {roleBasedRedirect};
+
