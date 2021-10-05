33165f520c97381fb4db18696b126cd82ef64c1d
dankk <dankkmemes@gmail.com>
2021-10-05 12:33:48 +0300

    loading page

--- client/src/pages/LoadingPage.js
+++ client/src/pages/LoadingPage.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+
+//https://tailwindcss.com/docs/animation
+//https://larainfo.com/blogs/tailwind-css-loading-spinner-example
+
 const LoadingPage = () => {
 
     return (
-        <h3>Load</h3>
+        // <h3>Load</h3>
+        <div className="flex h-screen justify-center pt-20">
+            <div style={{borderTopColor:"transparent"}}
+                className="w-16 h-16 border-4 border-gray-500 border-solid rounded-full animate-spin"></div>
+        </div>
     )
 }
 
