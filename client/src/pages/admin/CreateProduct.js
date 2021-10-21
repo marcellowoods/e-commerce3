54a1c5b4a03bf64b96f1547a13c996a30a1e9008
dankk <dankkmemes@gmail.com>
2021-10-21 15:26:51 +0300

    CreateProduct category  form

--- client/src/pages/admin/CreateProduct.js
+++ client/src/pages/admin/CreateProduct.js
@@ -1,4 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, Fragment, useEffect } from "react";
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 const PriceForm = ({ price, setPrice }) => {
     return (
@@ -83,6 +89,67 @@ const DescriptionForm = ({ description, setDescription }) => {
     )
 }
 
+
+
+const CategoryForm = ({ categories, selectedCategory, setSelectedCategory }) => {
+
+    return (
+        <div className="p-4">
+            <Listbox value={selectedCategory} onChange={setSelectedCategory}>
+                <Listbox.Label className="block text-sm font-medium text-gray-700">Category</Listbox.Label>
+                <div className="mt-1 relative">
+                    <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-1 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ease-linear transition-all duration-150">
+                        <span className="flex items-center">
+                            <span className="ml-3 block truncate">{selectedCategory.name}</span>
+                        </span>
+                        <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                            <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                        </span>
+                    </Listbox.Button>
+
+                    <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
+                        <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                            {categories.map((category) => (
+                                <Listbox.Option
+                                    key={category.id}
+                                    className={({ active }) =>
+                                        classNames(
+                                            active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                                            'cursor-default select-none relative py-2 pl-3 pr-9'
+                                        )
+                                    }
+                                    value={category}
+                                >
+                                    {({ selected, active }) => (
+                                        <>
+                                            <div className="flex items-center">
+                                                <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}>
+                                                    {category.name}
+                                                </span>
+                                            </div>
+
+                                            {selected ? (
+                                                <span
+                                                    className={classNames(
+                                                        active ? 'text-white' : 'text-indigo-600',
+                                                        'absolute inset-y-0 right-0 flex items-center pr-4'
+                                                    )}
+                                                >
+                                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                </span>
+                                            ) : null}
+                                        </>
+                                    )}
+                                </Listbox.Option>
+                            ))}
+                        </Listbox.Options>
+                    </Transition>
+                </div>
+            </Listbox>
+        </div>
+    )
+}
+
 //more forms
 //https://tailwindcomponents.com/component/account-card
 //https://tailwindcomponents.com/components/forms?page=2
@@ -91,6 +158,22 @@ const CreateProduct = () => {
     const [price, setPrice] = useState("");
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("");
+
+    useEffect(() => {
+
+        const c = [
+            { name: "watches", id: 21432 },
+            { name: "keyboards", id: 214132 }
+        ]
+
+        setCategories(c);
+        setSelectedCategory(c[0]);
+
+
+    }, [])
+
 
     return (
 
@@ -106,24 +189,18 @@ const CreateProduct = () => {
                 setPrice={setPrice}
             />
 
-            <DescriptionForm
-                description={description}
-                setDescription={setDescription}
+            <CategoryForm
+                categories={categories}
+                selectedCategory={selectedCategory}
+                setSelectedCategory={setSelectedCategory}
             />
 
 
 
-            {/* <div class="flex flex-wrap">
-                    <div class="w-full lg:w-12/12 px-4">
-                        <div class="relative w-full mb-3">
-                            <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlfor="grid-password">
-                                About me
-                            </label>
-                            <textarea type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" rows="4"> A beautiful UI Kit and Admin for JavaScript &amp; Tailwind CSS. It is Freeand Open Source.</textarea>
-                        </div>
-                    </div>
-                </div> */}
-
+            <DescriptionForm
+                description={description}
+                setDescription={setDescription}
+            />
 
         </div>
     )
