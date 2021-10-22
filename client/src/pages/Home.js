676231cf2cef9cf141bec8a1a633741f4ec299bb
dankk <dankkmemes@gmail.com>
2021-10-22 16:29:32 +0300

    font-normal

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -35,7 +35,7 @@ const CategoryCard = ({ handleClick, description, image, name, slug }) => {
             <div className="bg-gray-900 bg-opacity-50 flex items-center h-full">
                 <div className="px-10 max-w-xl">
                     <h2 className="text-2xl text-white font-semibold">{name}</h2>
-                    <p className="mt-2 text-white">{description}</p>
+                    <p className="mt-2  font-normal text-white">{description}</p>
                     <button onClick={() => handleClick(slug)} className="flex items-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
                         <span>Shop Now</span>
                         <svg className="h-5 w-5 mx-2" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
