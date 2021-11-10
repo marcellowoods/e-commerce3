adc5716a426fe61bece0dca130c0140ce797e23a
dankk <dankkmemes@gmail.com>
2021-11-10 14:34:41 +0200

    ListProducts

--- client/src/pages/admin/ListUsers.js
+++ client/src/pages/admin/ListUsers.js
@@ -1,20 +1,23 @@
 import React, { useState, Fragment, useEffect } from "react";
 
 /* This example requires Tailwind CSS v2.0+ */
-const products = [
+const people = [
     {
-        name: 'Casio',
-        price: 23,
-        quantity: 5,
+        name: 'Jane Cooper',
+        title: 'Regional Paradigm Technician',
+        department: 'Optimization',
+        role: 'Admin',
+        email: 'jane.cooper@example.com',
         image:
-            'https://m.media-amazon.com/images/I/61WixzlVuXL._UL1280_.jpg',
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
     },
+    // More people...
 ]
 
-const ListProducts = () => {
-
+const ListUsers = () => {
     return (
-            <div className="container mx-auto max-w-2xl pt-16 flex flex-col">
+
+            <div className="container mx-auto max-w-7xl pt-12 flex flex-col">
                 <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
@@ -31,13 +34,19 @@ const ListProducts = () => {
                                             scope="col"
                                             className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                                         >
-                                            Price
+                                            Title
                                         </th>
                                         <th
                                             scope="col"
                                             className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                                         >
-                                            Quantity
+                                            Status
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                        >
+                                            Role
                                         </th>
                                         <th scope="col" className="relative px-6 py-3">
                                             <span className="sr-only">Edit</span>
@@ -45,27 +54,29 @@ const ListProducts = () => {
                                     </tr>
                                 </thead>
                                 <tbody className="bg-white divide-y divide-gray-200">
-                                    {products.map((product) => (
-                                        <tr key={product.name}>
+                                    {people.map((person) => (
+                                        <tr key={person.email}>
                                             <td className="px-6 py-4 whitespace-nowrap">
                                                 <div className="flex items-center">
                                                     <div className="flex-shrink-0 h-10 w-10">
-                                                        <img className="h-10 w-10 rounded-full" src={product.image} alt="" />
+                                                        <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
                                                     </div>
                                                     <div className="ml-4">
-                                                        <div className="text-sm font-medium text-gray-900">{product.name}</div>
+                                                        <div className="text-sm font-medium text-gray-900">{person.name}</div>
+                                                        <div className="text-sm text-gray-500">{person.email}</div>
                                                     </div>
                                                 </div>
                                             </td>
                                             <td className="px-6 py-4 whitespace-nowrap">
-                                                <div className="text-sm text-gray-500">{product.price}</div>
+                                                <div className="text-sm text-gray-900">{person.title}</div>
+                                                <div className="text-sm text-gray-500">{person.department}</div>
                                             </td>
-
                                             <td className="px-6 py-4 whitespace-nowrap">
-                                                <div className="text-sm text-gray-500">{product.quantity}</div>
+                                                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                                    Active
+                                                </span>
                                             </td>
-    
-
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.role}</td>
                                             <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                                 <a href="#" className="text-indigo-600 hover:text-indigo-900">
                                                     Edit
@@ -83,4 +94,4 @@ const ListProducts = () => {
     )
 }
 
-export default ListProducts;
+export default ListUsers;
