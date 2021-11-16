7dd6919786de2a8610a98d5e16627a7933775b7e
dankk <dankkmemes@gmail.com>
2021-11-16 20:04:46 +0200

    Order checkout button

--- client/src/components/navigation/CartModal.js
+++ client/src/components/navigation/CartModal.js
@@ -71,12 +71,12 @@ const CartModal = () => {
 
                     <hr className="my-3" />
 
-                    <a onClick={handleCheckout} className={`cursor-pointer ${hasProducts() ? '' : 'cursor-not-allowed'} flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500`}>
+                    <button disabled={!hasProducts()} onClick={handleCheckout} className={`cursor-pointer ${hasProducts() ? '' : 'cursor-not-allowed'} flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500`}>
                         <span>
                             Checkout
                         </span>
                         <svg className="h-5 w-5 mx-2" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-                    </a>
+                    </button>
                 </div>
 
                 {isCartModalOpen() &&
