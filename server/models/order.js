de2e9565302f998fd1d323c3d0c677bbb6c16aec
dankk <dankkmemes@gmail.com>
2021-11-21 20:49:14 +0200

    orderSchema

--- server/models/order.js
+++ server/models/order.js
@@ -2,6 +2,36 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const { ObjectId } = mongoose.Schema;
 
+// const orderSchema = new mongoose.Schema(
+//     {
+//         products: [
+//             {
+//                 product: {
+//                     type: ObjectId,
+//                     ref: "Product",
+//                 },
+//                 count: Number,
+//                 color: String,
+//             },
+//         ],
+//         paymentIntent: {},
+//         orderStatus: {
+//             type: String,
+//             default: "Not Processed",
+//             enum: [
+//                 "Not Processed",
+//                 "processing",
+//                 "Cash On Delivery",
+//                 "Dispatched",
+//                 "Cancelled",
+//                 "Completed",
+//             ],
+//         },
+//         orderdBy: { type: ObjectId, ref: "User" },
+//     },
+//     { timestamps: true }
+// );
+
 const orderSchema = new mongoose.Schema(
     {
         products: [
@@ -14,20 +44,36 @@ const orderSchema = new mongoose.Schema(
                 color: String,
             },
         ],
-        paymentIntent: {},
         orderStatus: {
             type: String,
             default: "Not Processed",
             enum: [
                 "Not Processed",
-                "processing",
-                "Cash On Delivery",
+                "Processing",
                 "Dispatched",
                 "Cancelled",
                 "Completed",
             ],
         },
-        orderdBy: { type: ObjectId, ref: "User" },
+        orderedBy: { type: ObjectId, ref: "User" },
+        deliveryInfo: {
+            courrier: { type: String, required: true },
+            address: { type: String, required: true },
+            city: { type: String, required: true },
+            name: { type: String, required: true },
+            phone: { type: String, required: true },
+            email: { type: String, required: true },
+            method: {
+                type: String,
+                default: "home",
+                enum: [
+                    "home",
+                    "office"
+                ]
+            },
+            // postalCode: { type: String, required: true },
+            // country: { type: String, required: true },
+        }
     },
     { timestamps: true }
 );
