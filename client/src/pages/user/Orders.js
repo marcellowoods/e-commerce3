b75b37a8cd3e6df479155d248deca04e2193faa9
dankk <dankkmemes@gmail.com>
2021-11-29 14:15:59 +0200

    Order component

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -1,4 +1,5 @@
 import React from "react";
+import OrdersTable from "../../components/orders/OrdersTable";
 
 //test data
 const orders = [
@@ -22,61 +23,6 @@ const orders = [
     }
 ]
 
-const OrderRow = ({ orderId, orderDate, orderAddress, orderStatus, onDetailsClicked }) => {
-
-    return (
-        <tr className="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
-            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold">Order Id</span>{orderId}</td>
-            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold">Created on</span>{orderDate}</td>
-            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold">Shipping address</span>{orderAddress}</td>
-            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold">Status</span>{orderStatus}</td>
-            <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
-                {/* <span className="inline-block w-1/3 md:hidden font-bold">Actions</span> */}
-                {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Edit</button>
-                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Delete</button> */}
-                <button onClick={() => onDetailsClicked(orderId)} className="bg-blue-500 hover:bg-blue-700 text-white text-md font-bold py-1 px-2  rounded">
-                    Details
-                </button>
-            </td>
-        </tr>
-    )
-}
-
-const OrderTable = ({ onDetailsClicked }) => {
-
-
-    return (
-
-        <div className="container max-w-7xl mx-auto pt-12 px-6">
-            <table className="min-w-full border-collapse block md:table">
-                <thead className="block md:table-header-group">
-                    <tr className="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
-                        <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Order Id</th>
-                        <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Date</th>
-                        <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Address</th>
-                        <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Status</th>
-                        <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell"></th>
-                    </tr>
-                </thead>
-                <tbody className="block md:table-row-group">
-                    {orders.map((order) => {
-                        return (
-                            <OrderRow
-                                orderId={order.orderId}
-                                orderDate={order.orderDate}
-                                orderAddress={order.orderAddress}
-                                orderStatus={order.orderStatus}
-                                onDetailsClicked={onDetailsClicked}
-                            />
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
-    )
-
-}
-
 const Orders = () => {
 
     const onDetailsClicked = (orderId) => {
@@ -84,7 +30,8 @@ const Orders = () => {
     }
 
     return (
-        <OrderTable
+        <OrdersTable
+            orders={orders}
             onDetailsClicked={onDetailsClicked}
         />
     )
