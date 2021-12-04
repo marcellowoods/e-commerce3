faae4b86eb48586adb198a8d08fe65f32a264a25
dankk <dankkmemes@gmail.com>
2021-12-04 13:09:54 +0200

    StatusModal

--- client/src/pages/admin/UpdateOrders.js
+++ client/src/pages/admin/UpdateOrders.js
@@ -1,11 +1,104 @@
-import React, { useState } from "react";
+import React, { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationIcon } from '@heroicons/react/outline'
+
 import OrderDetails from "../../components/orders/OrderDetails";
 import { getOrders } from "../../functions/admin"
 import { useAsync } from "../../auxiliary/reactUtils"
 import { useSelector } from "react-redux";
+
 import LoadingPage from "../LoadingPage";
 
-const OrderRow = ({ orderId, orderDate, orderAddress, orderStatus, orderTotal, onDetailsClicked }) => {
+const StatusModal = ({ open, setOpen }) => {
+    // const [open, setOpen] = useState(true)
+
+    const cancelButtonRef = useRef(null)
+
+    return (
+        <Transition.Root show={open} as={Fragment}>
+            
+            {/* initialFocus={cancelButtonRef} */}
+            <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto"  onClose={setOpen}>
+                <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        {/* <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" /> */}
+                        <Dialog.Overlay className="fixed inset-0 " />
+
+                    </Transition.Child>
+
+                    {/* This element is to trick the browser into centering the modal contents. */}
+                    <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                        &#8203;
+                    </span>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        enterTo="opacity-100 translate-y-0 sm:scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    >
+                        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                <div className="sm:flex sm:items-start">
+                                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                        <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                                    </div>
+                                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                            Deactivate account
+                                        </Dialog.Title>
+                                        <div className="mt-2">
+                                            <p className="text-sm text-gray-500">
+                                                Are you sure you want to deactivate your account? All of your data will be permanently removed.
+                                                This action cannot be undone.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                                <button
+                                    type="button"
+                                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                                    onClick={() => setOpen(false)}
+                                >
+                                    Update
+                                </button>
+                                <button
+                                    type="button"
+                                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                    onClick={() => setOpen(false)}
+                                    ref={cancelButtonRef}
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                        </div>
+                    </Transition.Child>
+                </div>
+            </Dialog>
+        </Transition.Root>
+    )
+}
+
+const OrderRow = ({ orderId,
+    orderDate,
+    orderAddress,
+    orderStatus,
+    orderTotal,
+    onDetailsClicked,
+    onUpdateStatusClicked
+}) => {
 
     return (
         <tr className="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
@@ -31,10 +124,12 @@ const OrderRow = ({ orderId, orderDate, orderAddress, orderStatus, orderTotal, o
                 {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Edit</button>
                 <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Delete</button> */}
                 <div className="flex justify-between">
-                    <button onClick={() => onDetailsClicked(orderId)} className="bg-blue-500 hover:bg-blue-700 text-white text-md font-bold py-1 px-2  rounded">
+                    <button onClick={() => onDetailsClicked(orderId)}
+                        className="bg-blue-500 hover:bg-blue-700 text-white text-md font-bold py-1 px-2  rounded">
                         Details
                     </button>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">
+                    <button onClick={() => onUpdateStatusClicked(orderId)} 
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">
                         Update Status
                     </button>
                 </div>
@@ -45,7 +140,7 @@ const OrderRow = ({ orderId, orderDate, orderAddress, orderStatus, orderTotal, o
 
 
 //https://tailwindcomponents.com/component/responsive-table-6
-const OrdersTable = ({ onDetailsClicked, orders }) => {
+const OrdersTable = ({ onDetailsClicked, onUpdateStatusClicked, orders }) => {
 
     const getDate = (dateStr) => {
 
@@ -87,6 +182,7 @@ const OrdersTable = ({ onDetailsClicked, orders }) => {
                                 orderStatus={order.orderStatus}
                                 orderTotal={order.totalCost}
                                 onDetailsClicked={onDetailsClicked}
+                                onUpdateStatusClicked={onUpdateStatusClicked}
                             />
                         )
                     })}
@@ -119,10 +215,15 @@ const testOrders = [
     }
 ]
 
+
+
 const UpdateOrders = () => {
 
     const [orders, setOrders] = useState([]);
+
     const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+    const [isStatusOpen, setIsStatusOpen] = useState(false)
+
     const [isLoading, setIsLoading] = useState(false);
     const [orderSelected, setOrderSelected] = useState(null);
 
@@ -131,6 +232,11 @@ const UpdateOrders = () => {
 
     const onDetailsClicked = (orderId) => {
         setIsDetailsOpen((prev) => !prev);
+        setOrderSelected(orders.find(({ _id }) => _id == orderId));
+    }
+
+    const onUpdateStatusClicked = (orderId) => {
+        setIsStatusOpen((prev) => !prev);
         setOrderSelected(orders.find(({ _id }) => _id == orderId));
     }
 
@@ -161,9 +267,15 @@ const UpdateOrders = () => {
             )
             }
 
+            <StatusModal
+                open={isStatusOpen}
+                setOpen={setIsStatusOpen}
+            />
+
             <OrdersTable
                 orders={orders}
                 onDetailsClicked={onDetailsClicked}
+                onUpdateStatusClicked={onUpdateStatusClicked}
             />
         </div>
     )
