338a5e55102571b5d325d729f9f9f650b441638a
dankk <dankkmemes@gmail.com>
2021-12-16 12:17:23 +0200

    style pagination

--- client/src/components/navigation/getPagination.js
+++ client/src/components/navigation/getPagination.js
@@ -13,13 +13,13 @@ const getPagination = ({ pageCount, onPageChange, curPage }) => {
                 marginPagesDisplayed={0}
                 pageRangeDisplayed={2}
                 onPageChange={onPageChange}
-                containerClassName={"flex justify-center mt-10"}
+                containerClassName={"flex items-center justify-center mt-10"}
                 subContainerClassName={"flex rounded-md"}
-                previousLinkClassName={"py-2 px-4 leading-tight bg-white border border-gray-200 text-blue-700 border-r-0 ml-0 rounded-l hover:bg-blue-500 hover:text-white"}
-                nextLinkClassName={"py-2 px-4 leading-tight bg-white border border-gray-200 text-blue-700 rounded-r hover:bg-blue-500 hover:text-white"}
-                pageLinkClassName={"py-2 px-4 leading-tight bg-white border border-gray-200 text-blue-700 border-r-0 hover:bg-blue-500 hover:text-white"}
-                breakLinkClassName={"py-2 px-4 leading-tight bg-white border border-gray-200 text-blue-700 border-r-0 hover:bg-blue-500 hover:text-white"}
-                activeLinkClassName={"bg-gray-300"}
+                previousLinkClassName={"py-2 px-4 leading-tight bg-white font-normal  border-gray-200 text-blue-700 border-r-0 ml-0 rounded-l hover:bg-gray-50"}
+                nextLinkClassName={"py-2 px-4 leading-tight bg-white font-normal  border-gray-200 text-blue-700 rounded-r hover:bg-gray-50"}
+                pageLinkClassName={"py-2 px-4 leading-tight bg-white font-normal  border-gray-200 text-blue-700 border-r-0 hover:bg-gray-50 "}
+                breakLinkClassName={"py-2 px-4 leading-tight bg-white font-normal  border-gray-200 text-blue-700 border-r-0 hover:bg-gray-50 "}
+                activeLinkClassName={"font-semibold text-xl"}
             />
       
     )
