4083fd2cda6db560797b2053ac6705f925b8822d
dankk <dankkmemes@gmail.com>
2021-12-28 12:10:12 +0200

    MobileNavbarMenu close button

--- client/src/components/navigation/MobileNavbarMenu.js
+++ client/src/components/navigation/MobileNavbarMenu.js
@@ -24,7 +24,7 @@ const MobileNavbarMenu = (props) => {
     useEffect(() => {
 
         const handleResize = () => {
-            if(document.body.style.overflow != 'unset'){
+            if (document.body.style.overflow != 'unset') {
                 closeMobileNav();
             }
         }
@@ -37,12 +37,15 @@ const MobileNavbarMenu = (props) => {
             <div className="visible sm:invisible">
                 <div
                     className={`${isMobileNavOpen() ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'} z-40 fixed left-0 top-0  w-70 sm:w-full h-full px-6 py-4 transition duration-300 transform overflow-y-auto bg-white  `}>
+                    <div className="flex items-center justify-between">
+                        <ChangeLangDropdown />
+                        <button onClick={closeMobileNav} className="text-gray-600 focus:outline-none">
+                            <svg className="h-5 w-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M6 18L18 6M6 6l12 12"></path></svg>
+                        </button>
+                    </div>
                     <div className={`p-3`}>
 
                         <SearchBar cname={"mobile-searchbar-w relative py-3"} />
-                        <div className="py-2">
-                            <ChangeLangDropdown />
-                        </div>
                         <NavigationItems closeMobileNav={closeMobileNav} />
                     </div>
                 </div>
